1ddc9d27252b61783b645d95d6e752df3cec2a95
Omar Moyano <omarmoyano7w@gmail.com>
2023-08-30 01:38:16 -0300

    Create component Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="cuerpo">
+    <div class="container">
+      <h2>Iniciar Sesión</h2>
+      <form>
+        <div class="input-group">
+          <label for="username">Usuario</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <button class="btn" type="submit">Iniciar Sesión</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Login",
+//   props: {
+//     msg: String,
+//   },
+};
+</script>
+
+<style scoped>
+  .cuerpo {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 75vh;
+    background-color: #f0f0f0;
+  }
+
+  .container {
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    width: 100%;
+    max-width: 400px;
+    text-align: center;
+  }
+
+  .container h2 {
+    margin-bottom: 20px;
+  }
+
+  .input-group {
+    margin-bottom: 20px;
+  }
+
+  .input-group label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .input-group input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    max-width: 300px;
+  }
+
+  .input-group input:focus {
+    outline: none;
+    border-color: #007bff;
+  }
+
+  .btn {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+
+  .btn:hover {
+    background-color: #0056b3;
+  }
+</style>
