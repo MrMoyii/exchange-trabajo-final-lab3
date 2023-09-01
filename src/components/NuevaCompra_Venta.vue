52141ef4adf5b2afe2e5f9c55ac7f2de8753f51a
Omar Moyano <omarmoyano7w@gmail.com>
2023-08-31 23:06:25 -0300

    Add componente para compra/venta

---
+++ src/components/NuevaCompra_Venta.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="cuerpo">
+    <div class="container">
+      <h2>Seleccionar Compra o Venta</h2>
+      <div class="select-wrapper">
+        <select>
+          <option value="" disabled selected>Elige una opción</option>
+          <option value="compra">Compra</option>
+          <option value="venta">Venta</option>
+        </select>
+        <div class="arrow">▼</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuevaCompra-Venta",
+};
+</script>
+
+<style>
+.cuerpo {
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 97.5vh;
+  margin: 0;
+}
+
+.container {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+
+.select-wrapper {
+  display: inline-block;
+  position: relative;
+}
+
+select {
+  appearance: none;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  font-size: 16px;
+  width: 200px;
+  cursor: pointer;
+}
+
+select:focus {
+  outline: none;
+  border-color: #007bff;
+}
+
+.arrow {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+</style>
---
+++ src/views/Compra_VentaView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Compra_Venta />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Compra_Venta from "@/components/NuevaCompra_Venta.vue";
+
+export default {
+  name: "Compra_VentaView",
+  components: {
+    Compra_Venta,
+  },
+};
+</script>
